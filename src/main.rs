5766e99b52a8e098d43a7e69a7b79f311a3b38d5
Twil3akine <twil3akine@gmail.com>
2025-03-20 00:59:37 +0900

    UPDATE: main.rs - add Totros struct - add print function to print current grid status

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+#[allow(dead_code)]
+struct Totros {
+    width: usize,
+    height: usize,
+    grid: Vec<Vec<bool>>,
+}
+
+#[allow(dead_code)]
+impl Totros {
+    /*
+     Totrosの初期化
+     */
+    fn new() -> Self {
+        let w: usize = 10;
+        let h: usize = 16;
+
+        Totros {
+            width: w,
+            height: h,
+            grid: vec![vec![false; w]; h],
+        }
+    }
+
+    /*
+     grid全体を出力
+     */
+    fn print(&self) {
+        // 横線関数の定義
+        let write_line = |w: usize| {
+            print!("+");
+            for _ in 0..w { print!("-"); }
+            println!("+");
+        };
+
+        write_line(self.width);
+        // 縦ループ
+        for h in 0..self.height {
+            // 横ループ
+            print!("|");
+            for w in 0..self.width {
+                print!("{}", if self.grid[h][w] { '#' } else { ' ' });
+            }
+            println!("|");
+        }
+        write_line(self.width);
+    }
+}
+
+fn main() {
+    let mut totros = Totros::new();
+
+    let width: usize = totros.width;
+    let height: usize = totros.height;
+
+    totros.print();
+}
